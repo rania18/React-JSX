1182f0ee471fdf64000d0790a20553258485d9f5
rania18 <raniahamdi1994@gmail.com>
2021-03-22 16:14:39 +0100

    comment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
 import "./style.css";
 import car1 from "./images/car1.png";
+import Video from "./images/video.mp4";
 
 function App() {
   return (
@@ -15,9 +15,11 @@ function App() {
           <img src="/images/car2.png" />
         </div>
       </div>
-      <video width="320" height="240" controls>
+      {/* there are two ways to change tag video  */}
+      <video autostart autoPlay muted src={Video} type="video/mp4"  style={{width:700, height:240}} />
+      {/* <video style={{width:320, height:240}} controls>
         <source src="myVideo.mp4" type="video/mp4" />
-      </video>
+      </video> */}
     </div>
   );
 }
